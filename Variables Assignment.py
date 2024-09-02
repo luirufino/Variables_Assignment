657ac677d81219ae1abdcd0329a951ffa52adf0c
ADM <luigirufinosilva@gmail.com>
2024-09-02 18:02:56 -0400

    fun activity

---
+++ Variables Assignment.py
@@ -0,0 +1,12 @@
+first=5*6
+second=8/3
+third=8//3
+fourth=8%3
+fith=4200000 + 23090
+sixth=4200000 - 7633
+print("The answer to your query is",first)
+print("The answer to your query is",second)
+print("The answer to your query is",third)
+print("The answer to your query is",fourth)
+print("The answer to your query is",fith)
+print("The answer to your query is",sixth)
